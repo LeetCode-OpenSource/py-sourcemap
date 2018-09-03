b0579fa5b7c8cec6f0142ca68c937c03e708400d
Jerry Zou <jerry.zry@outlook.com>
2018-09-03 10:39:45 +0800

    style: format codes by yapf

---
+++ Makefile
@@ -0,0 +1,2 @@
+format:
+	yapf **/*.py -i
--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup
 from setuptools.command.install import install
 
+
 class PostInstallCommand(install):
     """Post-installation for installation mode."""
+
     def run(self):
         '''
         Write downloading process here
@@ -10,30 +12,30 @@ class PostInstallCommand(install):
         '''
         install.run(self)
 
+
 install_requires = []
 tests_require = install_requires + ['nose']
 
 setup(
-  name='py-sourcemap',
-  version='0.1',
-  packages=['py_sourcemap'],
-  classifiers=[
-    'Development Status :: 4 - Beta',
-    'Environment :: Console',
-    'Environment :: Web Environment',
-    'Intended Audience :: Developers',
-    'Operating System :: MacOS :: MacOS X',
-    'Operating System :: Microsoft :: Windows',
-    'Operating System :: POSIX',
-    'Programming Language :: Python',
-    'Programming Language :: Rust',
-  ],
-  install_requires=install_requires,
-  tests_require=tests_require,
-  test_suite='nose.collector',
-  cmdclass={
-    'install': PostInstallCommand,
-  },
-  # rust extensions are not zip safe, just like C-extensions.
-  zip_safe=False
-)
+    name='py-sourcemap',
+    version='0.1',
+    packages=['py_sourcemap'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Rust',
+    ],
+    install_requires=install_requires,
+    tests_require=tests_require,
+    test_suite='nose.collector',
+    cmdclass={
+        'install': PostInstallCommand,
+    },
+    # rust extensions are not zip safe, just like C-extensions.
+    zip_safe=False)
