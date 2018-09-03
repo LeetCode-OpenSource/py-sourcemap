225e78602258d80032deb0b15784ef23b92ab8da
Jerry Zou <jerry.zry@outlook.com>
2018-09-03 14:42:58 +0800

    chore: read host enviroment

--- setup.py
+++ setup.py
@@ -1,19 +1,42 @@
+import platform
+import os
+import sys
+
+from urllib.request import urlopen
+
 from setuptools import setup
 from setuptools.command.install import install
 
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+version = '0.1.10'
+local_build = os.environ.get('LOCAL', 0)
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
 
 
 class PostInstallCommand(install):
     """Post-installation for installation mode."""
 
     def run(self):
-        '''
-        Write downloading process here
-        mv target .so file into `py_sourcemap/py_sourcemap.so`
-        '''
+        version_tag = 'v{}'.format(version)
+        url_template = 'https://github.com/LeetCode-OpenSource/py-sourcemap/releases/download/{tag}/py_sourcemap.cpython-{py_ver}-{platform}.so'
+        py_version = '{}{}m'.format(sys.version_info.major,
+                                    sys.version_info.minor)
+        system = platform.system()
+        if system == 'Linux':
+            platform = 'x86_64-linux-gnu'
+        else:
+            raise Exception('This lib is only supporting Linux for now.')
+        download_url = url_template.format(tag=version_tag,
+                                           py_ver=py_version,
+                                           platform=platform)
+        dist = os.path.join(self.build_lib, 'py_sourcemap/py_sourcemap.so')
+        if not local_build:
+            with open(dist, 'wb') as f:
+                built_lib = urlopen(download_url).read()
+                f.write(built_lib)
         install.run(self)
 
 
@@ -22,7 +45,7 @@ tests_require = install_requires + ['nose']
 
 setup(
     name='py-sourcemap',
-    version='0.1.9',
+    version=version,
     packages=['py_sourcemap'],
     description='A tiny source-map-mappings bindings for python using PyO3',
     long_description=long_description,
